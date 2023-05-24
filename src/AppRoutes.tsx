9c27892c9b44e20bb4b661f5b017606516ae342e
Simon <s1175667@student.windesheim.nl>
2023-05-24 17:38:52 +0200

    Created basic file structure

---
+++ src/AppRoutes.tsx
@@ -0,0 +1,12 @@
+﻿import Main from "./pages/Main";
+
+const AppRoutes = [
+    {
+        path: '/',
+        pageName: 'Home',
+        element: <Main/>,
+        description: 'The landing page of the web application.'
+    }
+];
+
+export default AppRoutes;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,21 @@
+﻿import React from 'react'
+import './Style.css'
+
+const Main: React.FC = () => {
+    return (
+        <div>
+            <div className='home'>
+                <div className='home-content'>
+                    <div className='home-content__element'>
+                        <h1 className='home-content__element-welcome-message' data-cy='homepage-welcome-message'>
+                            {new Date().getHours() < 12 ? 'Good morning! ' : 'Hello! '}
+                            This is the home page!
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
