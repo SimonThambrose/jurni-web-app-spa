8d910f186ac254ca4197d273b46359b46279dc8c
Simon <s1175667@student.windesheim.nl>
2023-06-01 20:08:02 +0200

    Added Google Maps to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,50 +2,46 @@
 import {Link} from "react-router-dom";
 import {Col, Row} from "antd";
 import "./Footer.css";
+import FooterMap from "./FooterMap";
 
 const Footer: React.FC = () => {
     return (
         <div className='footer'>
             <Row className='footer-content'>
-                <Col className='footer-content__element' offset='1' span='17'>
+                <Col className='footer-content__element' offset='1' span='15'>
                     <Row className='footer-content__element-links'>
-                        <Col className='footer-content__element-links-jurni' data-cy='footer-links-jurni' span='6'>
-                            <p className='footer-head-jurni'>Jurni</p>
-                            <Link to='' className='footer-link-about-us'>About us</Link>
-                            <Link to=''>Lorem ipsum</Link>
-                            <Link to=''>Dolor sit</Link>
-                            <Link to=''>Amet consectetur</Link>
+                        <Col className='footer-content__element-links-jurni' data-cy='footer-links-jurni' span='7'>
+                            <h3 className='footer-head-jurni'>Jurni</h3>
+                            <Link to='' className='footer-link'>About us</Link>
+                            <Link to='' className='footer-link'>Lorem ipsum</Link>
+                            <Link to='' className='footer-link'>Dolor sit</Link>
+                            <Link to='' className='footer-link'>Amet consectetur</Link>
                         </Col>
-                        <Col className='footer-content__element-links-support' data-cy='footer-links-support' offset='1'
-                             span='6'>
-                            <p className='footer-head-support'>Support</p>
-                            <Link to=''>Lorem ipsum</Link>
-                            <Link to=''>Dolor sit</Link>
-                            <Link to=''>Amet consectetur</Link>
-                            <Link to=''>Adipiscing elit</Link>
+                        <Col className='footer-content__element-links-support' data-cy='footer-links-support' offset='1' span='7'>
+                            <h3 className='footer-head-support'>Support</h3>
+                            <Link to='' className='footer-link'>Lorem ipsum</Link>
+                            <Link to='' className='footer-link'>Dolor sit</Link>
+                            <Link to='' className='footer-link'>Amet consectetur</Link>
+                            <Link to='' className='footer-link'>Adipiscing elit</Link>
                         </Col>
-                        <Col className='footer-content__element-links-contact' data-cy='footer-links-contact' offset='1'
-                             span='6'>
-                            <p className='footer-head-contact'>Contact</p>
-                            <Link to='contact' className='footer-link-contact-us'>Contact us</Link>
-                            <Link to=''>Lorem ipsum</Link>
+                        <Col className='footer-content__element-links-contact' data-cy='footer-links-contact' offset='1' span='7'>
+                            <h3 className='footer-head-contact'>Contact</h3>
+                            <Link to='contact' className='footer-link'>Contact us</Link>
+                            <Link to='' className='footer-link'>Lorem ipsum</Link>
                         </Col>
                     </Row>
                 </Col>
-                <Col className='footer-content__element' offset='1' span='5'>
-                    <div className='footer-content__element-map'>
-
-                    </div>
+                <Col className='footer-content__element' offset='1' span='6'>
+                    <FooterMap/>
                 </Col>
                 <Col className='footer-content__element' span='24'>
                     <Row className='footer-content__element-disclaimers-row'>
-                        <Col className='footer-content__element-disclaimers-col' offset='1' span='11'>
-                            <p className='footer-copyright' data-cy='footer-copyright'>Jurni ©2023 Created by Simon
-                                Kuperus</p>
-                            <Link to=''>Terms & Conditions</Link>
-                            <Link to=''>Privacy Policy</Link>
-                            <Link to=''>Cookie Policy</Link>
-                            <Link to=''>Disclaimer</Link>
+                        <Col className='footer-content__element-disclaimers-col' data-cy='footer-disclaimers' offset='1' span='10'>
+                            <p className='footer-copyright'>Jurni © 2023 All rights reserved.</p>
+                            <Link to='' className='footer-link'>Terms & Conditions</Link>
+                            <Link to='' className='footer-link'>Privacy Policy</Link>
+                            <Link to='' className='footer-link'>Cookie Policy</Link>
+                            <Link to='' className='footer-link'>Disclaimer</Link>
                         </Col>
                     </Row>
                 </Col>
