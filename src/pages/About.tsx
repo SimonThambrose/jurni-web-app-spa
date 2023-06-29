88b37980116504e5128f72498060824086985eb7
Simon <s1175667@student.windesheim.nl>
2023-06-29 16:56:28 +0200

    Added like button with call to API to blogs on about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 ﻿import React, {useEffect, useState} from "react";
 import {BlogValues} from "../Models";
-import {fetchAllBlogs} from "../fetchers/BlogFetch";
+import {fetchAllBlogs, likeBlog} from "../fetchers/BlogFetch";
 import Header from "../components//Header/Header";
 import {Col, Row} from "antd";
 import Footer from "../components/Footer/Footer";
@@ -27,8 +27,10 @@ const About: React.FC = () => {
                 <Row className='about-content'>
                     <Col className='about-content__element' offset='1' span='22'>
                         <div className='about-content__element-blogs' data-cy='about-blogs'>
-                            {blogData.map(value =>
-                                <p>{`${value.title}: ${value.description}`}</p>
+                            {blogData.map(blog =>
+                                <p>{`${blog.title}: ${blog.description}`}
+                                    <button onClick={() => likeBlog(blog.id)}>Like ({blog.likes})</button>
+                                </p>
                             )}
                         </div>
                     </Col>
